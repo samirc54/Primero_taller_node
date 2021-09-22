9e23ba31705c90dafbd16add890a9430a2a25ed7
samirc54 <72276415+samirc54@users.noreply.github.com>
2021-09-21 20:39:02 -0500

    user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon')
+const user = require('./routes/user')
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -13,10 +14,10 @@ app.get("/", (req, res, next) =>
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next)=> {
     return res.status(404).json({code: 404, message: "URL no encontrada"});
-
 });
 
 app.listen(process.env.PORT || 3000, () => 
