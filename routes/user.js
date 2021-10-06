c89fca7bb6c2ff89b220c3424b8934dfc6edd3ce
samirc54 <72276415+samirc54@users.noreply.github.com>
2021-10-06 13:31:34 -0500

    2

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -23,15 +24,21 @@ user.post("/login", async (req, res, next) => {
     const {user_mail, user_password } = req.body;
     const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
     const rows = await db.query(query);
-
-    console.log(rows.length)
     
-    if (rows.length == 1) { 
-        return res.status(200).json({ code: 200, message: "Aquí irá el token" });
+    if (user_mail && user_password) {
+        if (rows.length == 1) { 
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+
+            return res.status(200).json({ code: 200, message: token });
     }
-    else {
-        return res.status(401).json({code: 401, message:"Usuario y/o contraseña incorrectos"}); 
+        else {
+            return res.status(401).json({code: 401, message:"Usuario y/o contraseña incorrectos"}); 
+        }
     }
+    return res.status(500).json({code: 500, message: "Campos incompletos" });
 });
 
 user.get("/", async (req, res, next) => {
